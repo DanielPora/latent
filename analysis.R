47f1495f9da47497a85f9f5db6bb23141ebb95ff
Daniel Porawski <d.porawski@posteo.de>
2023-03-28 12:02:26 +0200

    first commit
    first generation with ctgan
    and analysis.
    in the process to bring analysis.R in a presentable condition

---
+++ analysis.R
@@ -0,0 +1,267 @@
+library(dplyr)
+library(ggplot2)
+setwd("C:/Studium/BA Latent Groups")
+library(ggcorrplot)
+library(tidyverse)
+library('corrr')
+#install.packages("tidyverse")
+library("FactoMineR")
+library("factoextra")
+
+
+dat_tar <- read.csv("data_s3_target.csv")
+dat_id <- read.csv("data_s3_ID.csv")
+
+# clean_F.csv concerns only columns that are relevant for the analysis
+clean <- read.csv("clean_F.csv")
+
+
+# Preconsideration
+
+# Using a generative adversarial network (CTGAN) to generate synthetic data did
+# not proof to be viable. It was not able to pick up several important 
+# very strong correlations between columns. E.g. the one-hot encoding between 
+# own.cod and other.cod, target and response object have to be of the same test
+# type and so on. An example of this synthetic data can be found in synth1000.
+# It's planned to train this model again on a much more sparse predictor space
+# without the strong correlations and account for the multiple measurements
+# coming from the same participant
+synth1000 <- read.csv("synth1000.csv")
+
+# In order to generate authentic data a good understanding of the data and it's
+# distributions is necessary 
+
+
+
+# 1. Specific clusters regarding to the test
+subj_tests <- clean %>%
+  select("workerID", "aq_score_subset", "opt_score", "stroop_difference")%>%
+  distinct()
+
+# 1.1. Normalizing the tests data
+subj_tests_norm <- as.data.frame(scale(subj_tests[2:4]))
+subj_tests_norm <- subj_tests_norm%>%
+  mutate("workerID" = subj_tests$workerID)
+
+data_normalized <- subj_tests_norm %>%
+  select("workerID", "aq_score_subset", "opt_score", "stroop_difference")
+tests_norm <- data_normalized[,-1]
+rownames(tests_norm) <- data_normalized[,1]
+
+ggplot(stack(tests_norm), aes(x = ind, y = values)) +
+  stat_boxplot(geom = "errorbar", width = 0.25) +
+  labs(x="Test", y="Normalized Value") +
+  geom_boxplot() 
+
+# 1.2 Plotting densities of each test
+ggplot(tests_norm, aes(aq_score_subset))+
+  geom_density()
+
+ggplot(tests_norm, aes(opt_score))+
+  geom_density()
+
+ggplot(tests_norm, aes(stroop_difference))+
+  geom_density()
+
+# 1.3 Scatterplots of pairwise tests 
+ggplot(tests_norm, aes(aq_score_subset, opt_score))+
+  geom_point()
+
+ggplot(tests_norm, aes(aq_score_subset, stroop_difference))+
+  geom_point()
+
+ggplot(tests_norm, aes(stroop_difference, opt_score))+
+  geom_point()
+
+corr_matrix <- cor(tests_norm)
+corr_matrix
+
+# 1.4 Summary
+# The scatter plots don't hint at some correlation, using a correlation 
+# matrix to confirm.
+# Density of OPT shows a bi-modal distribution. Suggesting that participants
+# can be grouped in two optical perspective related groups.
+# There doesn't seem to be more of a structure. PCA was done in a seperated
+# analysis, but without any insightful result.
+ 
+
+# 2. Perspective taking preferences
+
+# 2.1. Identify problems for same perspective task
+problems <- clean %>%
+  filter(clean$accuracy == "0")
+# Front-Back problems
+fb_p <- problems %>%
+  filter(problems$targetPos == "F" |problems$targetPos == "B")
+# Left-Right problems
+lr_p <- problems %>%
+  filter(problems$targetPos == "L" |problems$targetPos == "R")
+nrow(fb_p)
+nrow(lr_p)
+
+clean %>%
+  count(perspective)
+
+# 2.2 Summary
+# In 1404 same perspective experiments occurred 1 left-right confusion (>0.001)
+# and 74 front-back confusions (=0.05)
+# This suggest that in about 5% of the cases for a "different" perspective front-back
+# scenario the own-other classification is not correct
+
+
+# 3 Individual perspective preference
+
+# 3.1 Relevant here are the "different" perspective scenarios
+
+# Using front-back, left-right... 
+fb <- clean %>%
+  filter(perspective == "different", targetPos == "F" | targetPos == "B")
+
+lr <- clean %>%
+  filter(perspective == "different", targetPos == "L" | targetPos == "R")
+
+# ... and all scenarios together to analyse the prefference by subject 
+by_subj_diff <- clean %>%
+  filter(perspective == "different") %>%
+  group_by(workerID) %>%
+  summarise(resp_diff = mean(respTime), sd_diff = sd(respTime), own_sum_diff = sum(own.cod), other_sum_diff = sum(other.cod))
+
+by_sub_fb <- fb %>%
+  group_by(workerID) %>%
+  summarise(resp_diff = mean(respTime), sd_diff = sd(respTime), own_sum_diff = sum(own.cod), other_sum_diff = sum(other.cod))
+
+by_sub_lr <- lr %>%
+  group_by(workerID) %>%
+  summarise(resp_diff = mean(respTime), sd_diff = sd(respTime), own_sum_diff = sum(own.cod), other_sum_diff = sum(other.cod))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+by_subj <- clean %>%
+  group_by(workerID) %>%
+  summarise(resp_all = mean(respTime), sd_all = sd(respTime), own_sum_all = sum(own.cod), other_sum_all = sum(other.cod))
+
+by_subj_same <- clean %>%
+  filter(perspective == "same") %>%
+  group_by(workerID) %>%
+  summarise(resp_same = mean(respTime), sd_same = sd(respTime), own_sum_same = sum(own.cod), other_sum_same = sum(other.cod))
+
+
+by_subj <- mutate(by_subj, total_sum_all = own_sum_all+other_sum_all)
+by_subj <- mutate(by_subj, direction_all = (own_sum_all-other_sum_all)/total_sum_all)
+
+by_subj_diff <- mutate(by_subj_diff, total_sum_diff = own_sum_diff+other_sum_diff)
+by_subj_diff <- mutate(by_subj_diff, direction_diff = (own_sum_diff-other_sum_diff)/total_sum_diff)
+
+by_subj_same <- mutate(by_subj_same, total_sum_same = own_sum_same+other_sum_same)
+by_subj_same <- mutate(by_subj_same, direction_same = (own_sum_same-other_sum_same)/total_sum_same)
+
+
+
+
+direction_density <- density(by_subj$direction_all)
+direction_density_diff <- density(by_subj_diff$direction_diff)
+direction_density_same <- density(by_subj_same$direction_same)
+
+
+plot(direction_density_diff, ylim = c(0,2), col = "blue")
+lines(direction_density, col = "red")
+lines(direction_density_same, col = "green")
+legend("topleft", legend = c("All trials", "same", "different"),
+       lwd = 3, col = c("red", "green", "blue"))
+
+
+hist(by_subj$direction_all, prob = TRUE, xlim = c(-1,1), ylim = c(0,10), col = "blue", breaks = 9)
+hist(by_subj_diff$direction_diff, prob = TRUE, add=TRUE, col = "red", breaks = 9)
+hist(by_subj_same$direction_same, prob = TRUE, add=TRUE, col = "green", breaks = 9)
+
+ggplot(by_subj, aes(direction_all))+
+  geom_histogram()
+  
+
+synth1000 <- read.csv("synth1000.csv")
+
+
+
+subj_tests <- clean %>%
+  select("workerID", "aq_score_subset", "opt_score", "stroop_difference")%>%
+  distinct()
+
+
+
+
+  
+  
+ggplot(subj_tests_norm, aes(aq_score_subset, opt_score, size = stroop_difference))+
+  geom_point()
+
+data_normalized <- subj_tests_norm %>%
+  select("workerID", "aq_score_subset", "opt_score", "stroop_difference")
+d2 <- data_normalized[,-1]
+rownames(d2) <- data_normalized[,1]
+
+corr_matrix <- cor(d2)
+corr_matrix
+
+
+
+data.pca <- princomp(corr_matrix)
+summary(data.pca)
+
+
+ 
+
+
+res.pca <- PCA(d2, graph = TRUE)
+
+ggplot(d2, aes(aq_score_subset))+
+  geom_density()
+
+ggplot(d2, aes(opt_score))+
+  geom_density()
+
+ggplot(d2, aes(stroop_difference))+
+  geom_density()
+
+ggplot(d2, aes(aq_score_subset, opt_score))+
+  geom_point()
+
+ggplot(d2, aes(aq_score_subset, stroop_difference))+
+  geom_point()
+
+ggplot(d2, aes(stroop_difference, opt_score))+
+  geom_point()
+
+pca <- prcomp(d2)
+rotated <- as.data.frame(scale(d2, pca$center, pca$scale) %*% pca$rotation)
+
+ggplot(d2, aes(aq_score_subset, opt_score, size = stroop_difference))+
+  geom_point()
+
+ggplot(rotated, aes(PC1, PC2, size = PC3))+
+  geom_point()
+
+pca2 <- prcomp(d2[c("stroop_difference", "aq_score_subset")])
+rotated2 <- as.data.frame(scale(d2[c("stroop_difference", "aq_score_subset")], pca2$center, pca2$scale) %*% pca2$rotation)
+
+summary(pca2)
+summary(pca)
+
+ggplot(rotated2, aes(PC1, PC2))+
+  geom_point()
